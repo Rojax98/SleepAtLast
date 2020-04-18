b2080f941b34244872fc366d74e0f39488f4a18f
ArtyAud <arthurdekerdrel@live.com>
2020-04-18 16:59:02 +0100

    Canvas added, narrative incoming, brace thyselves

--- Sleep at last/Assets/Scripts/enemyBarkScript.cs
+++ Sleep at last/Assets/Scripts/enemyBarkScript.cs
@@ -4,18 +4,28 @@ using UnityEngine;
 
 public class enemyBarkScript : MonoBehaviour
 {
+    //barks are dialogue spoken by enemies when chasing the player
     public string[] enemyBarks;
-    public List<string> myList = new List<string>();
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+        //sizing the bark array, adding dialogue
+        enemyBarks = new string[5];
+        enemyBarks[0] = "I can smell you.";
+        enemyBarks[1] = "You cannot escape!";
+        enemyBarks[2] = "Help me, it hurts...";
+        enemyBarks[3] = "Precious light.";
+        enemyBarks[4] = "Surrender your light to me";
+        //extra creepy >:3c
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        //this is to ensure all barks are present
+        Debug.Log(enemyBarks[0]);
+        Debug.Log(enemyBarks[1]);
+        Debug.Log(enemyBarks[2]);
+        Debug.Log(enemyBarks[3]);
+        Debug.Log(enemyBarks[4]);
     }
 }
