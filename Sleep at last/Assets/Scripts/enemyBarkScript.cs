a432126a9fc899b5858b0c771772d3d261fbda04
ArtyAud <arthurdekerdrel@live.com>
2020-04-18 16:21:28 +0100

    added enemyBarks
    barks could be used as pop ups when enemies run after the player c:

---
+++ Sleep at last/Assets/Scripts/enemyBarkScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyBarkScript : MonoBehaviour
+{
+    public string[] enemyBarks;
+    List<string> myList = new List<string>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
