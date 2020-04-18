c320d0c0046e9a9c88a4470a6eb17de4df2dd9ce
Rojax98 <58191485+Rojax98@users.noreply.github.com>
2020-04-18 15:28:57 +0100

    f

---
+++ Sleep at last/Assets/Scripts/enemyController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyController : MonoBehaviour
+{
+
+    GameObject Player;
+
+    public Light MainLight;
+
+
+    List<GameObject> Lights = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+
+
+    }
+
+    void InLight()
+    {
+
+
+
+
+    }
+
+    void FollowPlayer()
+    {
+
+        //edge 
+
+    }
+
+    void GoToBed()
+    {
+
+        //if ML < 20% go here
+
+    }
+}
